733d45faac361db981fa6adaa164e6f14f6bcca3
siddiq1-max <siddiqsaliyath@gmail.com>
2026-01-09 23:26:08 +0530

    Ready for Render deployment

--- ecommerce_project/settings.py
+++ ecommerce_project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-82f1@83j!x#b-_qsubbcjwhd1)85l!lj(4!qnkto7q_1blk0q2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
+ALLOWED_HOSTS = ['*']
 
-ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -118,6 +118,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
 STATICFILES_DIRS = [BASE_DIR / 'static']
 
 MEDIA_URL = '/media/'
